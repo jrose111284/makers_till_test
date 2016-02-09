2c9da86a5685ed20573592b49111782ad02d648e
jamierose <jrose111284@gmail.com>
2016-02-09 10:22:13 +0000

    can add more then one item and return total

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -7,4 +7,11 @@ describe Checkout do
     checkout.scan("Cafe Latte")
     expect(checkout.total).to eq(4.75)
   end
+
+  it 'can add more then one item and return total' do
+    checkout.scan("Cafe Latte")
+    checkout.scan("Single Espresso")
+    checkout.scan("Double Espresso")
+    expect(checkout.total).to eq(10.55)
+  end
 end
