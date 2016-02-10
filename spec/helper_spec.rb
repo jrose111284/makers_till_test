b6154dcab07e983fe1a9bbc47c7daf45e48aa6aa
jamierose <jrose111284@gmail.com>
2016-02-10 10:37:56 +0000

    added helper to shorten spec test

---
+++ spec/helper_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+def over_fifty
+  checkout.scan("Choc Mousse")
+  checkout.scan("Choc Mousse")
+  checkout.scan("Choc Mousse")
+  checkout.scan("Choc Mousse")
+  checkout.scan("Choc Mousse")
+  checkout.scan("Choc Mousse")
+  checkout.scan("Choc Mousse")
+end
