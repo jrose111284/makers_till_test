0801c067d4f2586ea511977a3d2f33190a78aa10
jamierose <jrose111284@gmail.com>
2016-02-10 10:36:34 +0000

    added function to added 5% discount

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,7 +1,7 @@
 require 'json'
 
 class Checkout
-
+  DISCOUNT = 50
   def initialize(items_json)
     @items = JSON.parse(File.read(items_json))
     @order = []
@@ -16,12 +16,17 @@ class Checkout
     @order.each do |shop_item|
       total_price += @items[shop_item]["product_price"]
     end
-    total_price
+    total_price.round(2)
   end
 
   def tax
     tax_percentage = @items["tax"]["tax_total"]
     total + tax_percentage
+  end
 
+  def apply_discount
+    discount_percentage = @items["discount_5%"]["discount_percentage"]
+    five_percent_off = total - discount_percentage
+    five_percent_off if total > DISCOUNT
   end
 end
