f90dd045f6044ebf2af3e117c7e084039eb312ae
jamierose <jrose111284@gmail.com>
2016-02-11 11:49:23 +0000

    refactored a little also added tax and discount into total to do it automatically

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -3,41 +3,37 @@ require 'checkout'
 describe Checkout do
   subject(:checkout) { described_class.new( './hipstercoffee.json') }
 
- context 'when adding up coffee items before tax' do
+ context 'when adding up coffee items' do
 
-  it 'can add price of coffee to order' do
+  it 'can add price of coffee to order with tax' do
     checkout.scan("Cafe Latte")
-    expect(checkout.total).to eq(4.75)
+    expect(checkout.total).to eq(5.15)
   end
 
   it 'can add more then one item and return total' do
     checkout.scan("Cafe Latte")
     checkout.scan("Single Espresso")
-    checkout.scan("Double Espresso")
-    expect(checkout.total).to eq(10.55)
+    expect(checkout.total).to eq(7.20)
   end
- end
-
- context 'when adding up coffee items with tax' do
-   it 'can add price of coffee to order and apply tax' do
-     checkout.scan("Cafe Latte")
-     checkout.total
-     expect(checkout.tax).to eq(5.15)
-   end
  end
 
  context 'gives 5% discount when you spend over £50' do
    it 'will give you 5 percent off over £50 spent' do
      over_fifty
-     checkout.total
-     checkout.tax
-     expect(checkout.apply_discount).to eq(57.25)
+     expect(checkout.total).to eq(57.65)
    end
  end
 
  context 'takes cash and gives change' do
     it 'take cash' do
-      expect(checkout.add_funds(10.00)).to eq(10.00)
+      expect(checkout.take_cash(10.00)).to eq(10.00)
+    end
+
+    it 'gives change cash' do
+      checkout.scan("Cafe Latte")
+      checkout.total
+      checkout.take_cash(10.00)
+      expect(checkout.give_change).to eq(4.85)
     end
   end
 end
